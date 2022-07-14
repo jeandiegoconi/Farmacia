f4710d9059d670b0f3a64c914f00a5729f1fedfa
jeandiegoconi <86448605+jeandiegoconi@users.noreply.github.com>
2022-07-14 19:48:30 -0400

    Orden general

--- checkout.php
+++ checkout.php
@@ -1,51 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <script src="https://www.paypal.com/sdk/js?client-id=Ae9l-0oCuI1vEcPWa_5qnZM6t-0V4iAV1oXGr7jMub37q_QBFCHkyZOiD7kWs8tPwox0QLeyWLKlEWFH">
+    <script
+        src="https://www.paypal.com/sdk/js?client-id=Ae9l-0oCuI1vEcPWa_5qnZM6t-0V4iAV1oXGr7jMub37q_QBFCHkyZOiD7kWs8tPwox0QLeyWLKlEWFH">
 
-    </script> 
+    </script>
 
 </head>
+
 <body>
 
-    <div id="paypal-button-container" ></div>
+    <div id="paypal-button-container"></div>
 
     <script>
-        paypal.Buttons({
-            style:{
-                color: "blue",
-                shape: "pill",
-                label: "pay"
-            },
-            createOrder: function(data, actions) {
-                return actions.order.create({
-                    purchase_units: [{
-                        amount:{ 
-                            value:100
-                        }
-                    }]
-                });
-            },
+    paypal.Buttons({
+        style: {
+            color: "blue",
+            shape: "pill",
+            label: "pay"
+        },
+        createOrder: function(data, actions) {
+            return actions.order.create({
+                purchase_units: [{
+                    amount: {
+                        value: 100
+                    }
+                }]
+            });
+        },
 
-            onApprove: function(data, actions) {
-                actions.order.capture().then(function (detalles){
-                    window.location.href="completado.html";
-                });
-            },
+        onApprove: function(data, actions) {
+            actions.order.capture().then(function(detalles) {
+                window.location.href = "completado.html";
+            });
+        },
 
-            onCancel: function(data) {
-                alert("Pago cancelado");
-                console.log(data);
-            
-            }
-        
+        onCancel: function(data) {
+            alert("Pago cancelado");
+            console.log(data);
 
-        }).render("#paypal-button-container");
+        }
+
+
+    }).render("#paypal-button-container");
     </script>
 </body>
+
 </html>
--- detalles.php
+++ detalles.php
@@ -11,7 +11,8 @@ $resultado = $sql -> fetchALL(PDO::FETCH_ASSOC);
 
 
 <!DOCTYPE html>
-<html lang= "es">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,7 +20,8 @@ $resultado = $sql -> fetchALL(PDO::FETCH_ASSOC);
     <title>Farmacias EcoProducts</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="css/estilos.css" rel="stylesheet">
 </head>
 
@@ -31,7 +33,8 @@ $resultado = $sql -> fetchALL(PDO::FETCH_ASSOC);
                 <a href="#" class="navbar-brand">
                     <strong>Farmacias EcoProducts</strong>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
+                    aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -53,11 +56,13 @@ $resultado = $sql -> fetchALL(PDO::FETCH_ASSOC);
     <!--Contenido-->
     <main>
         <div class="container">
-           
+
         </div>
     </main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 
 </body>
 
