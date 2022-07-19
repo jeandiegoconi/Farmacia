8cde41ee39c4caf03f3614da98e79c39657f3905
jeandiegoconi <86448605+jeandiegoconi@users.noreply.github.com>
2022-07-18 23:51:05 -0400

    Update categorias.php

--- controller/admin/categorias.php
+++ controller/admin/categorias.php
@@ -68,11 +68,33 @@ if (isset($_POST)) {
             </div>
         </div>
     </div>
+    <div id="categorias" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="my-modal-title"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-gradient-primary text-black">
+                    <h5 class="modal-title" id="title">Nueva Categoria</h5>
+
+                </div>
+                <div class="modal-body">
+                    <form action="" method="POST" autocomplete="off">
+                        <div class="form-group">
+                            <label for="nombre">Nombre</label>
+                            <input id="nombre" class="form-control" type="text" name="nombre" placeholder="Categoria"
+                                required>
+                        </div>
+                        <button class="btn btn-primary" type="submit">Registrar</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#categorias">
         Nuevo</button>
+    <a class="btn btn-primary" href="modificarCategorias.php" role="button">Modificar Categorias</a>
     <a class="btn btn-secondary" href="productos.php" role="button">Productos</a>
     <a class="btn btn-secondary" href="compras.php" role="button">Compras</a>
-    <a class="btn btn-secondary" href="modificarCategorias.php" role="button">Modificar Categorias</a>
     <a class="btn btn-danger float-end" href="cerrarSesion.php" role="button">Salir</a>
 </body>
 
